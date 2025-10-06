3da8f011e61ce209e2e5fa0bd0e2449c883b2bf3
Enes Akkaya <95829445+AkkayaEnes@users.noreply.github.com>
2025-10-06 19:42:28 +0300

    For Döngüsü ile karar yapıları, While döngüsü, Örnek Sınav Sorusu.

--- 04_Loops/Program.cs
+++ 04_Loops/Program.cs
@@ -64,17 +64,125 @@ namespace _04_Loops
             //}
             //Console.WriteLine(totalValue);
 
-            int totalValue= 0;
-            for (int i = 0; i < 20; i++)
-            {
-                if (i % 2 == 0)
-                {
-                     totalValue += i;
-                    Console.WriteLine(i);
-                }
-            }
-            Console.WriteLine("-------");
-            Console.WriteLine(totalValue);
+            //int totalValue= 0;
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //         totalValue += i;
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //Console.WriteLine("-------");
+            //Console.WriteLine(totalValue);
+
+
+            //int count = 0;
+            //for (int i = 1; i <= 50; i++)
+            //{
+            //    if (i % 7 == 0)
+            //    {
+            //        count++;
+            //        Console.WriteLine(i + " Sayısı 7 ile tam bölünür. 7 ile bölünen " + count + ".sayı");
+
+            //    }
+
+            //}
+            //Console.WriteLine("");
+            //Console.WriteLine("7 ile bölünen toplam sayı: " + count);
+
+
+            //24 saatte kaç bakteri olduğunu hesaplayan programı yaz
+
+            //int bakteriSayisi=1;
+
+            //for (int i = 1; i <= 24; i++)
+            //{
+            //    bakteriSayisi= bakteriSayisi * 2;
+            //    Console.WriteLine("1.Saatin sonunda bakteri sayısı: "+ bakteriSayisi);
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine(bakteriSayisi);
+
+            #endregion
+
+            #region While Döngüsü
+
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine("Merhaba Döngüler");
+            //    i++;
+            //}
+
+
+            //int i = 0;
+
+            //while (i <= 100)
+            //{
+            //    i++;
+            //    if (i % 3==0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //int i = 1;
+            //int result = 0;
+            //while (i <= 10)
+            //{
+
+            //     result = result + i;
+            //    i++;
+            //}
+            //Console.WriteLine(result);
+
+            #endregion
+
+            #region Örnek Sınav Sorusu
+
+            //Klavyeden girilen üç basamaklı sayının basamakları toplamını hesaplayan kodu yazınız.
+
+            //int sayi = 0;
+            //Console.WriteLine("Üç basamaklı sayı girişi: ");
+            //sayi = int.Parse(Console.ReadLine());
+
+            //int birlerBasamagi;
+            //int onlarBasamagi;
+            //int yuzlerBasamagi;
+
+
+            //if (sayi % 100 == 0)
+            //{
+            //    yuzlerBasamagi = sayi / 100;
+
+            //    Console.WriteLine("Sayının yüzler basamağı " + yuzlerBasamagi + "onlar ve  birler basamağı ");
+            //}
+            //else if (sayi % 100 != 0)
+            //{
+
+            //}
+
+            //Klavyeden girilen üç basamaklı sayının basamakları toplamını hesaplayan kodu yazınız. deneme 2
+
+            int sayi = 0;
+            Console.WriteLine("Üç basamaklı sayı girişi: ");
+            sayi = int.Parse(Console.ReadLine());
+
+            int birlerBasamagi;
+            int onlarBasamagi;
+            int yuzlerBasamagi;
+            yuzlerBasamagi = sayi / 100;
+            onlarBasamagi = sayi / 10;
+            onlarBasamagi = onlarBasamagi % 10;
+            birlerBasamagi = sayi % 10;
+
+            Console.WriteLine("Verdiğiniz sayının;");
+            Console.WriteLine("Birler basamağı: "+birlerBasamagi);
+            Console.WriteLine("Onlar basamağı: "+onlarBasamagi);
+            Console.WriteLine("Yüzler basamağı: "+yuzlerBasamagi);
+
+
             #endregion
 
             Console.Read();
