17557a1a7d5621e45a04547cd6a95d3d54423bab
Enes Akkaya <95829445+AkkayaEnes@users.noreply.github.com>
2025-10-07 23:48:31 +0300

    Foreach Döngüsü

---
+++ 07_ForeachLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Foreach Döngüsü
+            // Foreach (1;2;3;4)
+
+            //1:Değişken türü
+            //2: Değişken Adı,
+            //3:In
+            //4: Lsite, Koleksiyon, Dizi
+
+            #endregion
+        }
+    }
+}
